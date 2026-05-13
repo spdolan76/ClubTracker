eef78bb0fcbce8e003723fe2fe60f3529e7842c5
spdolan76 <spdolan76@gmail.com>
2026-05-13 08:01:23 -0400

    Update script.js
    Changed to match my club set

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const clubs = ["Driver", "3W", "5W", "Hybrid", "4I", "5I", "6I", "7I", "8I", "9I", "PW", "GW", "SW", "LW", "Putter"];
+const clubs = ["Driver", "3W", "4H", "4I", "5I", "6I", "7I", "8I", "9I", "PW", "GW", "SW", "LW", "Putter"];
 const container = document.getElementById('course-container');
 
 // Initialize 18 holes
